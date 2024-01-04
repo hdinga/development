af65d0d311391e4ba262888c9fd03a55e363e715
hdinga <59060005+hdinga@users.noreply.github.com>
2024-01-04 12:46:34 -0500

    chore: move social link icons into modular component chore: move email link into modular component chore: fix alignment and positioning of dropdown nav menu chore: fix responsiveness of scroll up arrow chore: apply styling updates to icons chore: add new email icon chore: remove date from footer chore: fix alignment and sizes of contents in footer chore: apply encrypted email formatting

--- build-001/src/components/_components.jsx
+++ build-001/src/components/_components.jsx
@@ -2,7 +2,9 @@
 import { useEffect } from "react";
 //
 import icons from "../components/_icons";
+import vars from "./_globalvars";
 import { ScrollToTop } from "../components/_interactions";
+//
 
 //-- [COMPONENT] NEW --
 export const BtnScrollUp = () => {
@@ -18,7 +20,40 @@ export const BtnScrollUp = () => {
   )
 }
 
+//-- [COMPONENT] EMAIL --
+export const EmailContact = () => {
+  const hiddenemail = atob(vars.brandEmail); // decoded email
+
+  return (
+    <a href={"mailto:"+hiddenemail+"?subject=General questions"} className="email-link">
+      { hiddenemail }
+    </a>
+  )
+}
+
+//-- [COMPONENT] SOCIAL LINKS --
+export const SocialLinks = () => {
+  // social links
+  const arrSocialLinks = [
+    {id: 1, name: "LinkedIn", icon: (icons.icn_linkedin), url: "https://linkedin.com/in/hdinga"},
+    {id: 2, name: "Github", icon: (icons.icn_github), url: "https://github.com/hdinga"},
+    {id: 3, name: "Instagram", icon: (icons.icn_instagram), url: "https://www.instagram.com/triggercell"},
+  ];
+
+  const listSocialLinks = arrSocialLinks.map((sociallink) =>
+    <a key={ sociallink.id } href={ sociallink.url } title={ sociallink.name } className="icn-social" target="_blank">
+      { sociallink.icon }
+    </a>
+  )
+
+  return (
+    <>{ listSocialLinks }</>
+  )
+}
+
 //-- [COMPONENT] NEW --
 export const NewComponent = () => {
   console.log("hello world!");
 }
+
+
